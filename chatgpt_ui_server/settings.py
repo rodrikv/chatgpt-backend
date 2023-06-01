7187f57d78df84385f4bd6f768bb19c643e0eec0
parsa <parsa@beastbrain.org>
2023-06-01 16:18:33 +0330

    some setting changes

--- chatgpt_ui_server/settings.py
+++ chatgpt_ui_server/settings.py
@@ -149,9 +149,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    "DEFAULT_RENDERER_CLASSES": (
-        # "rest_framework.renderers.BrowsableAPIRenderer",
-    ),
     "DEFAULT_AUTHENTICATION_CLASSES": [
         # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
         # 'rest_framework_simplejwt.authentication.JWTAuthentication'
